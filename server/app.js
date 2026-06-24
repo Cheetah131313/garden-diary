13aadda7ed6c873db19cf7a1d2796ead155b02b9
Anastasie Žďárská <153829307+Cheetah131313@users.noreply.github.com>
2026-06-24 11:46:15 +0200

    Fix environment variable casing for port

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 const userController = require("./controller/user");
 const plantController = require("./controller/plant");
